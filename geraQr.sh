77aabcc513f0a6675617d956af533632ad9424e1
Nobita X Shivam <66509772+Shivam304@users.noreply.github.com>
2023-04-03 01:37:22 +0530

    Add files via upload

---
+++ geraQr.sh
@@ -0,0 +1,3 @@
+z="
+";Fz='m'\''';Vz='rt.s';Az='LIGH';Iz='Y} g';Ez='0;37';Qz='rf V';Nz=' nov';Sz='Qr &';Lz='m qr';Dz='033[';Hz=' "${';Pz='rm -';Tz='& sh';Jz='eran';Bz='TGRA';Mz='code';Uz=' sta';Wz='h';Kz='do u';Gz='echo';Oz='o"';Rz='enom';Cz='Y='\''\';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Gz$Hz$Az$Bz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$z$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz"
